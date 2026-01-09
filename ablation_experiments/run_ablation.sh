d07688d77acb12df5659f532440dff4a2e79f3fa
Zian Luo <81810250+PSY99@users.noreply.github.com>
2026-01-09 09:13:42 +0800

    Update run_ablation.sh

--- ablation_experiments/run_ablation.sh
+++ ablation_experiments/run_ablation.sh
@@ -6,27 +6,27 @@ EVAL_SCRIPT="run_ablation.py"
 # Get dataset name from config for building directory name
 export DATASET_NAME="OpenSSH"
 
-echo "Starting batch evaluatfor $NUM_RUNS runs..."
+echo "Starting batch evaluation for $NUM_RUNS runs..."
 echo "=================================================="
 
-# Loop for specified number of times
+# Loop for the specified number of runs
 for i in $(seq 1 $NUM_RUNS)
 do
- # Format number to always be two digits (e.g., 01, 02, ..., 10)
- formatted_i=$(printf "%02d" $i)
+    # Format the number to always have two digits (e.g., 01, 02, ..., 10)
+    formatted_i=$(printf "%02d" $i)
 
- # Build artifacts directory suffix for this run
- export ARTIFACTS_SUFFIX="${DATASET_NAME}_${formatted_i}"
+    # Build the suffix for the artifacts directory for this run
+    export ARTIFACTS_SUFFIX="${DATASET_NAME}_${formatted_i}"
 
- echo ""
- echo "--- RUN $i/$NUM_RUNS ---"
- echo "Setting artifacts directory to: event_detector/generated_processors/${ARTIFACTS_SUFFIX}"
- 
- # Run evaluation script
- # Environment variable ARTIFACTS_SUFFIX will be read by python script
- python "$EVAL_SCRIPT"
+    echo ""
+    echo "--- RUN $i/$NUM_RUNS ---"
+    echo "Setting artifacts directory to: event_detector/generated_processors/${ARTIFACTS_SUFFIX}"
+    
+    # Run the evaluation script
+    # The environment variable ARTIFACTS_SUFFIX will be read by the python script
+    python "$EVAL_SCRIPT"
 
- echo "--- Finished RUN $i/$NUM_RUNS ---"
+    echo "--- Finished RUN $i/$NUM_RUNS ---"
 done
 
 echo ""
