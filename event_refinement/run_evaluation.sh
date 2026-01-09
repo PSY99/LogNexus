6c217daa081bd1041ecf406bd96201c9685d0d53
Zian Luo <81810250+PSY99@users.noreply.github.com>
2026-01-09 09:14:01 +0800

    Update run_evaluation.sh

--- event_refinement/run_evaluation.sh
+++ event_refinement/run_evaluation.sh
@@ -16,21 +16,21 @@ echo "=================================================="
 # specifytimesNumber
 for i in $(seq 1 $NUM_RUNS)
 do
- # Number，TotalYesNumber（ 01, 02, ..., 10）
- formatted_i=$(printf "%02d" $i)
+    # Number，TotalYesNumber（ 01, 02, ..., 10）
+    formatted_i=$(printf "%02d" $i)
 
- # BuildtimesRun artifacts Directory
- export ARTIFACTS_SUFFIX="${DATASET_NAME}_${formatted_i}"
+    # BuildtimesRun artifacts Directory
+    export ARTIFACTS_SUFFIX="${DATASET_NAME}_${formatted_i}"
 
- echo ""
- echo "--- RUN $i/$NUM_RUNS ---"
- echo "Setting artifacts directory to: event_detector/generated_processors/${ARTIFACTS_SUFFIX}"
- 
- # RunEvaluate
- # ARTIFACTS_SUFFIX python 
- python "$EVAL_SCRIPT"
+    echo ""
+    echo "--- RUN $i/$NUM_RUNS ---"
+    echo "Setting artifacts directory to: event_detector/generated_processors/${ARTIFACTS_SUFFIX}"
+    
+    # RunEvaluate
+    # ARTIFACTS_SUFFIX python 
+    python "$EVAL_SCRIPT"
 
- echo "--- Finished RUN $i/$NUM_RUNS ---"
+    echo "--- Finished RUN $i/$NUM_RUNS ---"
 done
 
 echo ""
